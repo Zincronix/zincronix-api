597a78fa603ee233b4606f48f4f374973d66c5de
Johel Saavedra <jbsch16@gmail.com>
2024-04-21 10:48:28 -0400

    [feat] Settings de sistema, implementado la fecha limite de reserva

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->insert([
+            [
+                'deadline_reservation' => "2024-10-10",
+                'type_reservation' => 'MANUAL',
+            ],
+
+        ]);
+    }
+}
