59efe3e3fb1c9ab08798353d29acba47dd724899
Johel Saavedra <jbsch16@gmail.com>
2024-05-07 07:53:50 -0400

    [fix] Teacher crud store

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\TeacherResource;
 use App\Models\DocenteMateriaGrupo;
 use App\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeacherController extends Controller
 {
@@ -30,25 +31,60 @@ class TeacherController extends Controller
      */ 
     public function store(CreateTeacherRequest $request)
     {
-        $teacher = new Teacher();
-        $teacher->name = $request->name;
-        $teacher->email = $request->email;
-        $teacher->password = '12345678';
-        $teacher->active = true;
+        try {
+            DB::beginTransaction();
 
-        $teacher->save();
+            $validated = $request->validated();
 
-        foreach ($request['subjects'] as $subjectInfo) {
+            $teacher = $this->createTeacher($validated);
+
+            $this->assignTeacher($teacher, $validated['subjects']);
+
+            DB::commit();
+
+            return response()->json(['message' => 'Teacher created successfully'], 201);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'status' => false,
+                'message' => 'Error al procesar la solicitud' . $e->getMessage()
+            ], 500);
+        }
+        
+    }
+
+    private function createTeacher(array $data)
+    {
+        return Teacher::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => '12345678',
+            'active' => true,
+        ]);
+    }
+
+    private function assignTeacher(Teacher $teacher, array $subjects)
+    {
+        foreach ($subjects as $subjectInfo) {
             foreach ($subjectInfo['groups'] as $groupId) {
+
+                $docenteMateriaGrupo = DocenteMateriaGrupo::where([
+                    'subject_id' => $subjectInfo['subject_id'],
+                    'group_id' => $groupId
+                ])->first();
+
+                if(!$docenteMateriaGrupo) {
+                    throw new \Exception('No hay consistencia en los datos de Subject o Group');
+                }
+
+                if ($docenteMateriaGrupo->teacher_id !== null){
+                    throw new \Exception('El grupo ya tiene docente asignado');
+                }
                 
-                $docenteMateriaGrupo = new DocenteMateriaGrupo();
                 $docenteMateriaGrupo->teacher_id = $teacher->id;
-                $docenteMateriaGrupo->subject_id = $subjectInfo['subject_id'];
-                $docenteMateriaGrupo->group_id = $groupId;
                 $docenteMateriaGrupo->save();
             }
         }
-        return response()->json(['message' => 'Teacher created successfully'], 201);
     }
 
     /**
