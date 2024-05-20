bcc2d460847750250c5028db1ac3f0aa412ac425
Johel Saavedra <jbsch16@gmail.com>
2024-05-20 11:12:21 -0400

    [feat] Vista correo con docente, materias y grupos

--- resources/views/emails/reservations/confirmar.blade.php
+++ resources/views/emails/reservations/confirmar.blade.php
@@ -4,46 +4,47 @@
 
 ## Estimado docente,
 
-Nos complace informarte que su solicitud de reserva ha sido aceptada. 
-A continuación, te proporcionamos los detalles de tu reserva:
+Nos complace informarle que su solicitud de reserva ha sido aceptada. 
+A continuación, le proporcionamos los detalles de su reserva:
 
 ## Información de la fecha y hora
 
-**Fecha de la solicitud:** {{ $date }}
+@component('mail::panel')
+**Fecha de la reserva:**        {{ $date }} <br>
+**Hora de la reserva:**         {{ $hour }}
+@endcomponent
 
-**Hora de la solicitud:** {{ $hour }}
 
-## Información del ambiente
+## Información del ambiente(s)
 
-{{-- 
+@component('mail::table')
 | Ambiente                  | Capacidad                     |
 |---------------------------|-------------------------------|
 @foreach ($classrooms as $classroom)
-| {{ $classroom->name }}    | {{ $classroom->capacity }}    |
-@endforeach --}}
+| {{ $classroom['name'] }}    | {{ $classroom['capacity'] }}    |
+@endforeach
+@endcomponent
 
 
-## Información del solicitante
+## Información del solicitante(s)
 
-{{-- @foreach ($docenteMateriaGrupo as $teacher)
+@foreach ($docenteMateriaGrupo as $teacher)
 **Nombre de docente**
 
 {{$teacher['teacher_name']}}
 
 **Materia(s) para la solicitud**
 
+@component('mail::table')
 | Materia(s)            | Grupo(s)                              |
 |-----------------------|---------------------------------------|
 @foreach ($teacher['subjects'] as $subject => $details)
-| {{ $subject }}        | {{ json_encode($details['groups']) }} |
+| {{ $subject }}        | {{ implode(', ', $details['groups']) }} |
 @endforeach
+@endcomponent
 
-@endforeach --}}
-
-| Encabezado 1              | Encabezado 2 |
-|---------------------------|--------------|
-| Fila 1 Col 1              | Fila 1 Col 2 |
-| Fila 2 Col 1              | Fila 2 Col 2 |
+---
+@endforeach
 
 @component('mail::button', ['url' => URL::to('/')])
 Mira tu reserva aquí
